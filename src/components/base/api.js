33c81dbddc44851a2408a1972cff1127e1ca5954
anonaeka <anonvisualdesign@gmail.com>
2021-07-30 11:53:42 +1000

    updatecommentzone and delete search bar

--- src/components/base/api.js
+++ src/components/base/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const Client = axios.create({
-    // baseURL: "https://safe-inlet-46917.herokuapp.com/api/v1/user"
-    baseURL: "http://localhost:4000/api/v1/user"
+    baseURL: "https://safe-inlet-46917.herokuapp.com/api/v1/user"
+    // baseURL: "http://localhost:4000/api/v1/user"
 });
 
 export default Client
